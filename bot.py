304bc87ed6bbd0d0a3b8d7e4b3d5a0050f94ecc0
MonkeyGTDev <monkeygtdev@gmail.com>
2026-07-06 13:19:22 -0400

    Downgrade py-cord to 2.4.1 to fix voice event loop bug

--- bot.py
+++ bot.py
@@ -71,9 +71,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        loop.run_until_complete(main())
-    finally:
-        loop.close()
+    asyncio.run(main())
